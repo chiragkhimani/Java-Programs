d595351bf78e7ac62cfd72a731085d48475aebdd
khimanichirag <khimanichirag@gmail.com>
2020-12-30 06:38:31 +0530

    Updated Class 15

--- src/com/java/class15/GitHubExamples.java
+++ src/com/java/class15/GitHubExamples.java
@@ -2,6 +2,7 @@ package com.java.class15;
 
 public class GitHubExamples {
 	public static void main(String[] args) {
-		
+		System.out.println("This is from Chirag");
+		System.out.println("My task is completed for today");
 	}
 }
