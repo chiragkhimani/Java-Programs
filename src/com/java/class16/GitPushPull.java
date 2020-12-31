c0ce37ca7a29f2cdef027d6a2c74ae023df2ce94
unknown <mkaraduman0@gmail.com>
2020-12-30 16:26:57 -0800

    this is mehmets commit

--- src/com/java/class16/GitPushPull.java
+++ src/com/java/class16/GitPushPull.java
@@ -3,5 +3,6 @@ package com.java.class16;
 public class GitPushPull {
 	public static void main(String[] args) {
 		System.out.println("Git pull and push");
+		System.out.println("Mehmet has pushed");
 	}
 }
